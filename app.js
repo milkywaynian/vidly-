630699dbd81aeb831b1f96abf5e14abe42cdda14
milkywaynian <marina.faraji1@gmail.com>
2020-07-20 11:04:22 +0100

    add get request and port listening

---
+++ app.js
@@ -0,0 +1,19 @@
+//load express
+const express = require("express"); 
+
+//store result of the express module in a variable
+const app = express();
+
+
+
+
+// create dummy categories
+
+const categories = [
+    {name: "comedy"},
+    {name: "thriller"},
+    {name: "drama"}
+]
+
+//get method 
+
