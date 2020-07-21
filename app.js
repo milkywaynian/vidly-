97ba1d3fc9aeacafac5f84df2ae13300db0aa34b
milkywaynian <marina.faraji1@gmail.com>
2020-07-21 11:08:11 +0100

    add get request for individual genre

--- app.js
+++ app.js
@@ -6,21 +6,36 @@ const app = express();
 
 
 
-// create dummy categories
+// create dummy genres
 
-const categories = [
-    {name: "comedy"},
-    {name: "thriller"},
-    {name: "drama"}
+const genres = [
+    {name: "comedy", id:1},
+    {name: "thriller", id:2},
+    {name: "drama", id:3}
 ]
 
 //get method 
 
-app.get('/app/categories', (req,res) => {
-    res.send(categories)
+app.get('/app/genres', (req,res) => {
+    res.send(genres)
 })
 
+//get method for an individual genre
 
+app.get('/app/genres/:name', (req, res) => {
+    //find the genre
+    const genre = genres.find((g) => g.name === (req.params.name));
+
+    //provide a response
+    //if none found
+    console.log(genre);
+    if(!genre) {
+        res.status(404).send("Genre not found"); 
+        //if genre found
+    } else {
+        res.send(genre); 
+    }
+})
 
 
 
